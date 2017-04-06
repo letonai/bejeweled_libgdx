d72b51dadfc59bcec99ff54245548a2c2bb6da34
letonai <letonai@gmail.com>
2017-04-06 11:59:10 -0300

    imports player class

--- gdx-game-android/src/br/letonai/jewels/Player.java
+++ gdx-game-android/src/br/letonai/jewels/Player.java
@@ -1,7 +1,10 @@
 package br.letonai.jewels;
 
-import com.badlogic.gdx.Gdx;
-import 
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.math.*;
+import android.view.*;
 
 public class Player
 {
