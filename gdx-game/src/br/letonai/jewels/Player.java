07900c791626fb003c9f7210a05b0e56f2b0efe3
letonai <letonai@gmail.com>
2017-04-06 14:02:18 -0300

    fixed inputprocessor

--- gdx-game/src/br/letonai/jewels/Player.java
+++ gdx-game/src/br/letonai/jewels/Player.java
@@ -8,7 +8,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.TimeUtils;
 
-public class Player extends GameObject implements InputProcessor{
+public class Player extends GameObject implements InputProcessor {
 	
 	private Sprite sp;
 	private Texture texture;
@@ -17,6 +17,54 @@ public class Player extends GameObject implements InputProcessor{
 		super(x,y);
 		this.texture=txt;
 	}
+	
+	@Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+
+        return true;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        if (allowJump && isDragged == false) {
+            allowJump = false;
+            jump(); 
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        if(isDragged==false) {
+        isDragged = true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
 	
 
 }
