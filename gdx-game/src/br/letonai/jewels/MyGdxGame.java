760089e480358668c093dd90ccf29a6a8e4ae173
letonai <letonai@gmail.com>
2017-04-07 09:22:34 -0300

    sadadas

--- gdx-game/src/br/letonai/jewels/MyGdxGame.java
+++ gdx-game/src/br/letonai/jewels/MyGdxGame.java
@@ -54,7 +54,7 @@ public class MyGdxGame implements ApplicationListener
 		player.setX(touchPos.x-player.getTexture().getWidth()/2);
 		//player.setX(MathUtils.sin(t*player.getY())-player.getTexture().getWidth()/2);
 		//player.setY((Gdx.graphics.getHeight()-touchPos.y)-player.getTexture().getHeight()/2);
-		player.setY((player.getY()*MathUtils.sin(t))-player.getTexture().getHeight()/2);
+		player.setY((player.getY()*MathUtils.sin(System.currentTimeMillis()))-player.getTexture().getHeight()/2);
 		Log.i("bejeweled","SIN: "+(player.getY()*MathUtils.sin(t)))
 		batch.end();
 	}
