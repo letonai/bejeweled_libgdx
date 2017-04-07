b1c839deafc249e36be78492fc15fd1c71a2906a
letonai <letonai@gmail.com>
2017-04-07 09:07:21 -0300

    sdds

--- gdx-game/src/br/letonai/jewels/MyGdxGame.java
+++ gdx-game/src/br/letonai/jewels/MyGdxGame.java
@@ -26,9 +26,6 @@ public class MyGdxGame implements ApplicationListener
 		player = new GameObject(ball);
 		//player.setTexture(ball);
 		//Sprite s = new Sprite(ball);
-		
-
-		
 	}
 
 	@Override
@@ -39,13 +36,12 @@ public class MyGdxGame implements ApplicationListener
 		float t=(float) TimeUtils.millis();
 		
 		if(false && Gdx.input.isTouched()){
-			
 			touchPos.set(Gdx.input.getX(),Gdx.input.getY(),0);
-			
-			}else{
-				touchPos.set(Gdx.graphics.getWidth() / 4, 
-					   Gdx.graphics.getWidth() / 2, Gdx.graphics.getWidth() / 2);
-			}
+		}else{
+			touchPos.set(Gdx.graphics.getWidth() / 4, 
+		    Gdx.graphics.getWidth() / 2, Gdx.graphics.getWidth() / 2);
+		}
+		
 	    Gdx.gl.glClearColor(0, 1, 1, 1);
 	    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
@@ -55,8 +51,9 @@ public class MyGdxGame implements ApplicationListener
 		player.draw(batch);
 		
 		player.setX(touchPos.x-player.getTexture().getWidth()/2);
+		//player.setX(MathUtils.sin(t*player.getY())-player.getTexture().getWidth()/2);
 		//player.setY((Gdx.graphics.getHeight()-touchPos.y)-player.getTexture().getHeight()/2);
-		player.setY((Gdx.graphics.getHeight()-MathUtils.sin(t*player.getX()))-player.getTexture().getHeight()/2);
+		player.setY((MathUtils.sin(t*player.getX()))-player.getTexture().getHeight()/2);
 		batch.end();
 	}
 
