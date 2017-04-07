9fc063bda9ceb2282c12b228a744a8fa1b426320
letonai <letonai@gmail.com>
2017-04-07 09:33:22 -0300

    asddsa

--- gdx-game/src/br/letonai/jewels/MyGdxGame.java
+++ gdx-game/src/br/letonai/jewels/MyGdxGame.java
@@ -16,7 +16,7 @@ public class MyGdxGame implements ApplicationListener
 	SpriteBatch batch;
 	GameObject player;
 	Sprite sprite;
-	int count;
+	float count;
 	//teste celular
 
 	@Override
@@ -56,7 +56,7 @@ public class MyGdxGame implements ApplicationListener
 		player.setX(touchPos.x-player.getTexture().getWidth()/2);
 		//player.setX(MathUtils.sin(t*player.getY())-player.getTexture().getWidth()/2);
 		//player.setY((Gdx.graphics.getHeight()-touchPos.y)-player.getTexture().getHeight()/2);
-		player.setY((player.getY()*MathUtils.sin(TimeUtils.millis()*count++))-player.getTexture().getHeight()/2);
+		player.setY((player.getY()*MathUtils.sin(count+=0.1))-player.getTexture().getHeight()/2);
 		Log.i("bejeweled","SIN: "+(player.getY()*MathUtils.sin(t)));
 		batch.end();
 	}
